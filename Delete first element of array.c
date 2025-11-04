fdc926c6eb92af41bfe597fb8ceae93a11d63887
PushpaCS02 <pushpaCS7997@gmail.com>
2025-11-04 19:52:31 +0530

    Create Delete first element of array.c

---
+++ Delete first element of array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define CAP 10
+
+int main() {
+    int a[CAP] = {1, 2, 3, 4, 5};
+    int n = 5 ;
+    if(n>0){
+        for(int i=0;i<n;i++)
+        a[i]=a[i+1];
+        n--;
+    }else{
+        printf("array is empty");
+    }
+    for(int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\npushpa s d\n");
+    printf("1SV24CS109");
+    return 0;
+}
