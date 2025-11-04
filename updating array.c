68cdcace6bc14b51dfecb3e02c1df25aa8aef9d3
PushpaCS02 <pushpaCS7997@gmail.com>
2025-11-04 15:24:49 +0530

    Create updating array.c

---
+++ updating array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+
+    int a[]={1,2,3,4,5};
+    int n=sizeof(a)/sizeof(a[0]);
+    int index=2;
+    int newval=41;
+    if(index>=0 && index<= n)
+    {
+        a[index]=newval;
+        }
+    for(int i=0;i<n;i++){
+        printf("%d ",a[i]);
+    }
+    else{
+        printf("out of range");
+    }
+    return 0;
+}
